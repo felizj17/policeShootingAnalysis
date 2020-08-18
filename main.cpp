c2c376978407229d4aae07699611ae666d9953fd
felizj17 <felizj17@gmail.com>
2020-08-18 01:16:29 -0400

    added censusData csv file and created function for data extraction

--- main.cpp
+++ main.cpp
@@ -4,60 +4,22 @@
 #include <string>
 #include <vector>
 using namespace std;
+void dataExtract(Case a[], int b, string c);
 int rowCount(string a);
 void bodyCamPercent(Case a[], double b, double c[]);
 void racePercent(Case a[], double b, double c[]);
 void stateRacePopComp(Case a[], double b, double c[]);
 
 int main() {
-	string fs;
-	int i = 0;
-	fs = "C:\\Users\\feliz\\Downloads\\policeshootings.csv";
-	char str[64];
+	string ps,cd;
+	ps = "policeshootings.csv";
+	cd = "CensusData2017.csv";
 	double race_percentages[6];
 	double body_cam_percentages[3];
-	string data[15];
-	ifstream a;
-	a.open(fs);
-	if (a.is_open()) {
-		cout << "File is Open\n";
-	}
-	else {
-		cout << "File not found!\n Try entering full path of document with '\' replaced with '\\' \n";
-	}
-	//nor will store the number of lines extracted from the file
-	int nor = rowCount(fs);
-	//change to vector of type Case arrays not as easy to manipulate 
+	int nor = rowCount(ps);
 	Case* b = NULL;
 	b = new Case[nor];
-	/*======================================================================================================================================================*/
-	//currently troubleshooting
-	/*======================================================================================================================================================*/
-	for(int i =0;i<nor;i++){
-		for ( int j = 0;j < 15;j++) {
-			a.getline(str, 64, ',');
-			data[j] = str;
-			
-		}
-		//assigning the data to the Case class variables 
-		b[i].setId(data[0]);
-		b[i].setName(data[1]);
-		b[i].setDate(data[2]);
-		b[i].setMod(data[3]);
-		b[i].setWeapon(data[4]);
-		b[i].setAge(data[5]);
-		b[i].setGender(data[6]);
-		b[i].setRace(data[7]);
-		b[i].setCity(data[8]);
-		b[i].setState(data[9]);
-		b[i].setSomi(data[10]);
-		b[i].setThreatlevel(data[11]);
-		b[i].setFleeing(data[12]);
-		b[i].setBody_cam(data[13]);
-		b[i].setArms(data[14]);		
-	}
-	
-	
+	dataExtract(b, nor, ps);	
 	racePercent(b, nor, race_percentages);
 	for (int f = 0; f < 6; f++) {
 		cout << race_percentages[f] << '\n';
@@ -169,4 +131,40 @@ int rowCount(string a)
 	}
 	ds.close();
 	return count;
+}
+void dataExtract(Case a[], int b, string c) {
+	ifstream ds;
+	char str[64];
+	string data[15];
+	ds.open(c);
+	if (ds.is_open()) {
+		cout << "File is Open\n";
+	}
+	else {
+		cout << "File not found!\n Try entering full path of document with '\' replaced with '\\' \n";
+	}
+	for (int i = 0; i < b; i++) {
+		for (int j = 0; j < 15; j++) {
+			ds.getline(str, 64, ',');
+			data[j] = str;
+
+		}
+		//assigning the data to the Case class variables 
+		a[i].setId(data[0]);
+		a[i].setName(data[1]);
+		a[i].setDate(data[2]);
+		a[i].setMod(data[3]);
+		a[i].setWeapon(data[4]);
+		a[i].setAge(data[5]);
+		a[i].setGender(data[6]);
+		a[i].setRace(data[7]);
+		a[i].setCity(data[8]);
+		a[i].setState(data[9]);
+		a[i].setSomi(data[10]);
+		a[i].setThreatlevel(data[11]);
+		a[i].setFleeing(data[12]);
+		a[i].setBody_cam(data[13]);
+		a[i].setArms(data[14]);
+	}
+	ds.close();
 }
